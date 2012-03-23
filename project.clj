5a9685cc2eb01c4f921473e555a07763b9700956
Evan Mezeske <evan@meebo-inc.com>
2012-03-22 21:00:01 -0700

    Shameless plug of the lein-cljsbuild plugin.

--- project.clj
+++ project.clj
@@ -2,5 +2,11 @@
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [noir "1.2.1"]]
+            :plugins [[lein-cljsbuild "0.1.3"]]
+            :cljsbuild {
+              :builds [{:source-path "tempest"
+                        :compiler {:output-to "resources/public/tempest.js"
+                                   :optimizations :simple
+                                   :pretty-print true}}]}
             :main tempest-cljs.server)
 
