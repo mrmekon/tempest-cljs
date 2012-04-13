0233fa2926deeb2fc9a3ac17388c1972c9f76f27
mrmekon <mrmekon@gmail.com>
2012-04-13 19:03:31 -0400

    Added license to project file, switched to newer version of cljsbuild, and to less optimization for now.  Tested with advanced optimization, and it works.

--- project.clj
+++ project.clj
@@ -2,14 +2,18 @@
   :description "Clone of Tempest vector-graphic arcade game.
                 Written in ClojureScript, and uses HTML5 2D canvas
                 for display."
+  :license {:name "Modified BSD License"
+            :url "http://www.opensource.org/licenses/BSD-3-Clause"
+            :distribution :repo}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [noir "1.2.1"]]
   :dev-dependencies [[lein-marginalia "0.7.0-SNAPSHOT"]]
-  :plugins [[lein-cljsbuild "0.1.6"]]
+  :plugins [[lein-cljsbuild "0.1.7"]]
   :cljsbuild {
               :builds [{:source-path "tempest"
                         :compiler {:output-to "resources/public/tempest.js"
-                                   :optimizations :simple
+                                   :optimizations :whitespace
+                                   ;;:optimizations :advanced
                                    :pretty-print true}}]}
   :main tempest-cljs.server)
 
